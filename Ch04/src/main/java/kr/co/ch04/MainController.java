5b117f63dacded1602cfecafd392b3f350eb7c3a
stellalk <yeeunkk@gmail.com>
2021-04-13 13:01:26 +0900

    04/13/2021 Spring MVC

---
+++ Ch04/src/main/java/kr/co/ch04/MainController.java
@@ -0,0 +1,30 @@
+package kr.co.ch04;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping(value = {"/", "/index"})
+	public String index() {
+		return "index";
+	}
+	
+	@RequestMapping("/hello")
+	public String hello() {
+		return "hello";
+	}
+	
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "welcome";
+	}
+	
+	@PostMapping("/greeting")
+	public String greeting() {
+		return "greeting";
+	}
+}
